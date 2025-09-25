af8acb23fc995de5c1a065e5bc5978ab716db1b0
Benji Cervantes <benjicervantes11@gmail.com>
2025-09-24 20:08:43 -0600

    Infraestructure created

---
+++ src/AppointmentApp.Domain/Entities/Appointment.cs
@@ -0,0 +1,15 @@
+﻿namespace AppointmentApp.Domain.Entities
+{
+    public class Appointment
+    {
+        public int AppointmentId { get; set; }
+        public DateTime AppointmentDate { get; set; }
+        public string AppointmentTitle { get; set; }
+        public string AppointmentDescription { get; set; }
+        public string AppointmentType { get; set; }
+        public bool Attended { get; set; }
+        public User Doctor { get; set; }
+        public Patient Patient { get; set; }
+        
+    }
+}
---
+++ src/AppointmentApp.Domain/Entities/Patient.cs
@@ -0,0 +1,11 @@
+﻿namespace AppointmentApp.Domain.Entities
+{
+    public class Patient
+    {
+        public int PatientId { get; set; }
+        public string PatientName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string PatientDescription { get; set; }
+    }
+}
